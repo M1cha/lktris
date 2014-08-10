978c0bf1b8c6535197f04cabfbf39cea7d445b31
M1cha <sigmaepsilon92@gmail.com>
2014-08-10 22:15:07 +0200

    libc: rand: use glibc version of rand

--- lib/libc/rand.c
+++ lib/libc/rand.c
@@ -43,7 +43,27 @@ void rand_add_entropy(const void *buf, size_t len)
 	randseed ^= enp;
 }
 
-int rand(void)
+int
+rand (void)
 {
-	return (randseed = randseed * 1664525 + 1013904223);
+  unsigned int next = randseed;
+  int result;
+
+  next *= 1103515245;
+  next += 12345;
+  result = (unsigned int) (next / 65536) % 2048;
+
+  next *= 1103515245;
+  next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (next / 65536) % 1024;
+
+  next *= 1103515245;
+  next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (next / 65536) % 1024;
+
+  randseed = next;
+
+  return result;
 }
